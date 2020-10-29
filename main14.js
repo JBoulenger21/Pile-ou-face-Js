6ff66f06810db934f1cfff991dcbc84380671195
JBoulenger21 <boulenger.julie@gmail.com>
2020-10-29 09:30:43 +0100

    acceleration de l'animation

--- main14.js
+++ main14.js
@@ -2,12 +2,12 @@ const balanceIcon = document.getElementById('balanceIcon');
 
 function balanceScale() {
  balanceIcon.innerHTML = "&#xf240";
- setTimeout(function(){balanceIcon.innerHTML = "&#xf241";}, 1000);
- setTimeout(function(){balanceIcon.innerHTML = "&#xf242";}, 2000);
- setTimeout(function(){balanceIcon.innerHTML = "&#xf243";}, 3000);
- setTimeout(function(){balanceIcon.innerHTML = "&#xf244";}, 4000);
+ setTimeout(function(){balanceIcon.innerHTML = "&#xf241";}, 500);
+ setTimeout(function(){balanceIcon.innerHTML = "&#xf242";}, 1000);
+ setTimeout(function(){balanceIcon.innerHTML = "&#xf243";}, 1500);
+ setTimeout(function(){balanceIcon.innerHTML = "&#xf244";}, 2000);
 }
 
 balanceScale();
 
-setInterval(balanceScale, 5000);
+setInterval(balanceScale, 2500);
